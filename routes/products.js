a4b55331127f4fe670a4ff97177dce90c7647f47
Samar AYADI <ayadisamardev@gmail.com>
2023-01-27 11:01:56 +0100

    Project Ecommerce Backend with PinguCoder

---
+++ routes/products.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const { userById } = require("../middlewares/user");
+const router = express.Router();
+
+const {
+  allProducts,
+  relatedProduct,
+  createProduct,
+  showProduct,
+  productById,
+  removeProduct,
+  updateProduct,
+  SearchProduct,
+  photoProduct
+} = require("../controllers/productController");
+
+router.get('/', allProducts);
+
+const { requireSignIn, isAuth, isAdmin } = require("../middlewares/auth");
+
+router.get("/:productId", showProduct);
+
+router.get('/related/:productId', relatedProduct);
+
+router.post("/create/:userId", [requireSignIn, isAuth, isAdmin], createProduct);
+
+router.post('/search', SearchProduct);
+
+router.get('/photo/:productId', photoProduct);
+
+router.put('/:productId/:userId', [requireSignIn, isAuth, isAdmin], updateProduct)
+
+router.delete('/:productId/:userId', [requireSignIn, isAuth, isAdmin], removeProduct)
+
+
+router.param("userId", userById);
+router.param("productId", productById);
+module.exports = router;
